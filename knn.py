93ab838eb828cd43ff627820871be5d0aaf4af56
Iria Duran Lusquiños <irdulus@gmail.com>
2022-11-21 01:09:35 +0100

    Revert "Adding Vote Policies"
    This reverts commit b49ea15c2975823b97f700ac85d87d4e251dbf19.

--- knn.py
+++ knn.py
@@ -72,29 +72,23 @@ class KNN:
         #return distances
         return
 
-    def vote(labels, number_classes, Y, voting, distances=None):
-        voting_list = []
+    def vote(self, labels, number_classes, distances):
+        voting_matrix = [[] for j in range(len(labels))]
         for i_x, nearest_labels in enumerate(labels):
-            voting_class_list = []
             for class_y in range(number_classes):
-                if voting == 'majority':
+                # TO DO
+                if self.voting == 'majority':
+                    # voting_value_class: suma de votos de la clase en la que se itera (class_y)
+                    # para la muestra sobre la que se itera (i_x). Asegurarse de que siempre es un
+                    # valor real voting_value_class
+                    voting_value_class = sum(1 for i in nearest_labels if i == class_y) # algo así, no se si estará bien
+                #elif self.voting == 'inverse_distance':
+                # hay que usar la matriz de distancias; distancia entre la sample i_x y el
+                # vecino j es distances[i_x, j] dnd j pertenece a nearest_labels
+                    #voting_value_class =
+                #elif self.voting == 'shepards':
+                    #voting_value_class =
+                voting_matrix[i_x].append(voting_value_class)
 
-                    voting_value_class = sum(1 for i in nearest_labels if i == class_y)
-
-                elif voting == 'inverse_distance':
-                    voting_value_class = sum(1 / abs(distances(i_x, i)) for i in nearest_labels if i == class_y)
-
-                elif voting == 'shepards':
-                    voting_value_class = sum(exp(- abs(distances(i_x, i))) for i in nearest_labels if i == class_y)
-                voting_class_list.append(voting_value_class)
-
-            winners = np.argwhere(voting_class_list == np.max(voting_class_list)).flatten()
-            if len(winners) > 1:  # tie
-                total_class = []
-                for winner_class in winners:
-                    total_class.append(sum(1 for i in Y if i == winner_class))
-
-                voting_list.append(winners[np.argmax(total_class).flatten()][0])
-            else:  # non tie
-                voting_list.append(winners[0])
-        return voting_list
+        voting_matrix = np.array(voting_matrix)
+        return np.armax(voting_matrix, axis = 1)
