f89a0d62d86febe68eace00c51a2dfd00abff0d2
Javi GG <s.javi.com@gmail.com>
2022-11-28 09:33:45 +0100

    modified knn.py, added utils.py

--- knn.py
+++ knn.py
@@ -1,10 +1,11 @@
 import numpy as np
 from sklearn.feature_selection import mutual_info_classif
 import sklearn_relief as relief
-from scipy.spatial.distance import euclidean
 from sklearn.neighbors import KDTree
 from sklearn.preprocessing import normalize
-
+from scipy.spatial.distance import cdist
+from utils import *
+from math import exp
 
 class KNN:
     def __init__(self, n_neighbors=5,
@@ -12,160 +13,105 @@ class KNN:
                  metric='minkowski',
                  voting='majority',
                  p=1,
+                 distances_precomputed = None,
+                 weights_precomputed = None,
+                 metric_scipy = True
                  ):
 
         self.k = n_neighbors
+        if voting not in ['majority', 'inverse_distance', 'shepards']:
+            raise ValueError("voting is expected to be named as; 'majority', 'inverse_distance' or 'shepards', got {} instead".format(metric))
         self.voting = voting
         self.weights = weights
+        if metric not in ['minkowski', 'euclidean', 'cosine']:
+            raise ValueError("metric is expected to be named as; 'minkowski', 'euclidean' or 'cosine', got {} instead".format(metric))
+        if metric == 'minkowski' and p <= 0:
+            raise ValueError("p must be greater than 0")
         self.metric = metric
         self.p = p
+        self.distances = distances_precomputed
+        self.metric_scipy = metric_scipy
+        self.W = weights_precomputed
 
     def fit(self, X, Y):
         self.X = X
         self.Y = Y
-        self.compute_weights()  # crea self.W (los pesos)
+        if self.W is None:
+            self.compute_weights()
 
     def predict(self, X_new):
         N = X_new.shape[0]
-        distances = self.computeDistanceMatrix(X_new, self.X, self.W)
+        if self.distances is None:
+            self.distances = self.computeDistanceMatrix(X_new)
         # compute k nearest neighbours for each instance
-        knn_indexes = [np.argsort(distances[i, :])[:self.k] for i in range(N)]
-        labels_of_neighbours = [self.Y[indexes].astype(
-            np.int) for indexes in KNN_indexes]
-        N_c = len(self.Y.unique())
-        y_pred = self.vote(labels_of_neighbours, N_c, distances)
+        knn_indexes = [np.argsort(self.distances[i, :])[:self.k] for i in range(N)]
+        labels_of_neighbours = [self.Y[indexes].astype(np.int) for indexes in knn_indexes]
+        N_c = np.unique(self.Y).shape[0]
+        y_pred = self.vote(labels_of_neighbours, N_c)
         return y_pred
 
     def fit_predict(self, X, Y):
         return self.fit(X, Y).predict(X)
 
-    def find_nm(self, sample, X):
-        dist = 100000
-        idx = None
-        for i, s in enumerate(X):
-            tmp = euclidean(sample, s)
-            if tmp <= dist:
-                dist = tmp
-                idx = i
-        return idx
-
-    def relief(self):
-
-        feature_scores = np.zeros(self.X.shape[1])
-
-        labels, counts = np.unique(self.Y, return_counts=True)
-        Prob = counts / float(len(self.Y))
-        for label in labels:
-            # Find the nearest hit for each sample in the subset with the corresponding label
-            select = (self.Y == label)
-            tree = KDTree(self.X[select, :])
-            nh = tree.query(self.X[select, :], k=2, return_distance=False)[:, 1:]
-            nh = (nh.T[0]).tolist()
-
-            # Calculate the difference of x with nh
-            nh_mat = np.square(np.subtract(
-                self.X[select, :], self.X[select, :][nh, :])) * -1
-
-            # Find the nearest miss for each sample in the other subset
-            nm_mat = np.zeros_like(self.X[select, :])
-            for prob, other_label in zip(Prob[labels != label], labels[labels != label]):
-                other_select = (self.Y == other_label)
-                nm = []
-                for sample in self.X[select, :]:
-                    nm.append(find_nm(sample, self.X[other_select, :]))
-                    
-                # # Calculate the difference of x with nm
-                nm_tmp = np.square(np.subtract(
-                    self.X[select, :], self.X[other_select, :][nm, :])) * prob
-                nm_mat = np.add(nm_mat, nm_tmp)
-
-            mat = np.add(nh_mat, nm_mat)
-            feature_scores += np.sum(mat, axis=0)
-
-        return normalize([feature_scores])
-
     def compute_weights(self):
-        # TO DO haciendo uso del self.X y del self.Y
-        # weights va a ser un vector de dimensiones self.X.shape[1] --> numero de columnas (o variables)
-
         if self.weights == 'uniform':
-            self.W = np.ones((1, self.X.shape[1]))
+            self.W = np.ones((self.X.shape[1],))
 
         elif self.weights == 'info_gain':
             self.W = mutual_info_classif(self.X, self.Y)
 
         elif self.weights == 'relief':
-            self.W = relief(self.X, self.Y)
+            self.W = relief.ReliefF().fit(self.X, self.Y).w_
 
-    def computeDistanceMatrix(self, X_new, X, W):
-        # TO DO
-        # las funciones de distancia q implementemos se pueden poner en otro archivo py
-        # e importarlas desde aquí o implementarlas aqui
-        # if self.metric == 'euclidean':
-        #distances = euclidean_dist(X_new, X, w=W)
-        # similar a lo que hace la funcion scipy.spatial.distance.cdist
-        # pero implementado de forma manual
-        # distances será una matriz de dimensiones lxn donde:
-        # - l --> numero de instancias de la matriz X_new
-        # - n --> numero de instancias de la matriz X
-        # cada elemento de la matriz distances D[i, j]
-        # sera la distancia del elemento i al elemento j, siendo
-        # i una muestra de la matriz X_new y j una muestra de la matriz X
-        # OJO: distancia ponderada: hay que usar la matriz W
-        # if self.metric == 'minkowski':
-        # usar el self.p
-        # distances = minkowski_dist(X_new, X, w=W)
-        # if self.metric == 'cosine':
-        # distances = cosine_dist(X_new, X, w=W)
-        # return distances
-        return
+    def computeDistanceMatrix(self, X_new):
 
-    def computeDistancesMatrix(metric, X_new, X, W, p):
-        if (metric == 'euclidean' or metric == 'minkowski') and p <= 0:
-            raise ValueError("p must be greater than 0")
+        if self.metric == 'euclidean':
+            if self.metric_scipy:
+                dist_matrix = cdist(X_new, self.X, metric=self.metric, p=2, w=self.W)
+            else:
+                #dist_matrix = euclidean_matrix(X_new, X, W, p)
+                pass
 
-        if validate_dimensions(X_new, X) == 0:
-            if validate_dimensions(X_new, W) == 0:
+        elif self.metric == 'minkowski':
+            if self.metric_scipy:
+                dist_matrix = cdist(X_new, self.X, metric=self.metric, p=self.p, w=self.W)
+            else:
+                #dist_matrix = minkowski_matrix(X_new, X, W, p)
+                pass
 
-                if metric == 'euclidean':
-                    dist_matrix = euclidean_matrix(X_new, X, W, p)
+        elif self.metric == 'cosine':
+            if self.metric_scipy:
+                dist_matrix = cdist(X_new, self.X, metric=self.metric, w=self.W)
+            else:
+                # dist_matrix = minkowski_matrix(X_new, X, W, p)
+                pass
 
-                elif metric == 'minkowski':
-                    dist_matrix = minkowski_matrix(X_new, X, W, p)
+        return dist_matrix
 
-                elif metric == 'cosine':
-                    dist_matrix = cosine_matrix(X_new, X)
-
-                elif metric == 'w_cosine':
-                    dist_matrix = weighted_cosine_matrix(X_new, X, W)
-
-                D = dist_matrix
-
-                return D
-
-    def vote(labels, number_classes, Y, voting, distances=None):
+    def vote(self, labels, number_classes, distances=None):
         voting_list = []
+
         for i_x, nearest_labels in enumerate(labels):
             voting_class_list = []
             for class_y in range(number_classes):
-                if voting == 'majority':
-
+                if self.voting == 'majority':
                     voting_value_class = sum(1 for i in nearest_labels if i == class_y)
-
-                elif voting == 'inverse_distance':
-                    voting_value_class = sum(1 / abs(distances(i,i_x)) for i in nearest_labels if i == class_y)
-
-                elif voting == 'shepards':
-                    voting_value_class = sum(exp(- abs(distances(i,i_x))) for i in nearest_labels if i == class_y)
-                voting_class_list.append(voting_value_class)
+                    voting_class_list.append(voting_value_class)
+                elif self.voting == 'inverse_distance':
+                    voting_value_class = sum(1 / abs( self.distances[i,i_x] ) for i in nearest_labels if i == class_y)
+                    voting_class_list.append(voting_value_class)
+                elif self.voting == 'shepards':
+                    voting_value_class = sum(exp(- abs( self.distances[i,i_x] )) for i in nearest_labels if i == class_y)
+                    voting_class_list.append(voting_value_class)
 
             winners = np.argwhere(voting_class_list == np.max(voting_class_list)).flatten()
+
             if len(winners) > 1:  # tie
                 total_class = []
                 for winner_class in winners:
-                    total_class.append(sum(1 for i in Y if i == winner_class))
-
+                    total_class.append(sum(1 for i in self.Y if i == winner_class))
                 voting_list.append(winners[np.argmax(total_class).flatten()][0])
             else:  # non tie
                 voting_list.append(winners[0])
-        return voting_list
+
+        return np.array(voting_list)
--- utils.py
+++ utils.py
@@ -1,5 +1,46 @@
 import numpy as np
+from scipy.spatial.distance import euclidean
 
+def find_nm(sample, X):
+    dist = 100000
+    idx = None
+    for i, s in enumerate(X):
+        tmp = euclidean(sample, s)
+        if tmp <= dist:
+            dist = tmp
+            idx = i
+    return idx
+
+def relief(X, Y):
+    feature_scores = np.zeros(X.shape[1])
+    labels, counts = np.unique(Y, return_counts=True)
+
+    prob = counts / float(len(Y))
+    for label in labels:
+        # Find the nearest hit for each sample in the subset with the corresponding label
+        select = (Y == label)
+        tree = KDTree(X[select, :])
+        nh = tree.query(X[select, :], k=2, return_distance=False)[:, 1:]
+        nh = (nh.T[0]).tolist()
+
+        # Calculate the difference of x with nh
+        nh_mat = np.square(np.subtract(
+            X[select, :], X[select, :][nh, :])) * -1
+
+        # Find the nearest miss for each sample in the other subset
+        nm_mat = np.zeros_like(X[select, :])
+        for prob, other_label in zip(prob[labels != label], labels[labels != label]):
+            other_select = (Y == other_label)
+            nm = []
+            for sample in X[select, :]:
+                nm.append(find_nm(sample, X[other_select, :]))
+            # Calculate the difference of x with nm
+            nm_tmp = np.square(np.subtract(
+                X[select, :], X[other_select, :][nm, :])) * prob
+            nm_mat = np.add(nm_mat, nm_tmp)
+        mat = np.add(nh_mat, nm_mat)
+        feature_scores += np.sum(mat, axis=0)
+    return normalize([feature_scores])
 
 # Validate dimensions of matrices
 def validate_dimensions(u, v):
@@ -30,34 +71,27 @@ def validate_weights(u, w):
 # Functions for minkowski
 def minkowski(u, v, w, p):
     u_v = np.abs(u - v)
-
     m = 0
-    for i in range(len(u)):
+    for i in range(u.shape[0]):
         m = m + (u_v[i] ** p) * w[i]
-
     m = m ** (1 / p)
     return m
-
 
 def minkowski_matrix(u, v, w, p):
     if p <= 0:
         raise ValueError("p must be greater than 0")
-
-    if validate_dimensions(u, v) == 0:
-        if validate_weights(u, w) == 0:
-
-            D = np.zeros((len(u[0]), len(v)))
-            for j in range(len(u[0])):
-                for i in range(len(u)):
-                    D[i][j] = minkowski(u[i], v[j], w[i], p)
-            return D
-
+    N = u.shape[0]
+    N_prima = v.shape[0]
+    D = np.zeros((N, N_prima))
+    for i in range(N):
+        for j in range(N_prima):
+            D[i][j] = minkowski(u[i], v[j], w, p)
+    return D
 
 # Function for euclidean
 def euclidean_matrix(u, v, w, p):
     euclidean_dist = minkowski_matrix(u, v, w, p=2)
     return euclidean_dist
-
 
 # Functions for cosine
 def cosine(u, v, w=None):
@@ -71,7 +105,6 @@ def cosine(u, v, w=None):
     cos = uv / (norm_u * norm_v)
     return cos
 
-
 def cosine_matrix(u, v):
     if validate_dimensions(u, v) == 0:
         D = np.zeros((len(u[0]), len(v)))
@@ -79,7 +112,6 @@ def cosine_matrix(u, v):
             for i in range(len(u)):
                 D[i][j] = cosine(u[i], v[j])
         return D
-
 
 # Weighted cosine
 def weighted_cosine(u, v, w):
@@ -94,7 +126,6 @@ def weighted_cosine(u, v, w):
         w_cos = w_cos + ((w[i] * u[i] * v[i]) / (norm_u * norm_v))
     return w_cos
 
-
 def weighted_cosine_matrix(u, v, w):
     if validate_dimensions(u, v) == 0:
         if validate_weights(u, w) == 0:
@@ -103,5 +134,3 @@ def weighted_cosine_matrix(u, v, w):
                 for i in range(len(u)):
                     D[i][j] = weighted_cosine(u[i], v[j], w[i])
         return D
-
-
